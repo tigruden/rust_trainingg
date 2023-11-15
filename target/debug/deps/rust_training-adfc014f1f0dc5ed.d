18be329e7c1a4e250e148034b0ac21b8e59e2dc9
Burak Maleri <b.maleri@gmail.com>
2023-11-15 23:31:33 +0300

    rust project initialize

---
+++ target/debug/deps/rust_training-adfc014f1f0dc5ed.d
@@ -0,0 +1,5 @@
+/Users/burakmaleri/Desktop/Test Project/rust_trainingg/target/debug/deps/rust_training-adfc014f1f0dc5ed.rmeta: src/main.rs
+
+/Users/burakmaleri/Desktop/Test Project/rust_trainingg/target/debug/deps/rust_training-adfc014f1f0dc5ed.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/deps/rust_training-b5f4f0e241af4add.d
@@ -0,0 +1,5 @@
+/Users/burakmaleri/Desktop/Test Project/rust_trainingg/target/debug/deps/rust_training-b5f4f0e241af4add.rmeta: src/main.rs
+
+/Users/burakmaleri/Desktop/Test Project/rust_trainingg/target/debug/deps/rust_training-b5f4f0e241af4add.d: src/main.rs
+
+src/main.rs:
